196531469f5ee51f5cf7114891e641392ba38871
ingridmejia <50503752+ingridmejia@users.noreply.github.com>
2019-05-15 16:41:00 -0500

    Update matri2x2.py

--- matri2x2.py
+++ matri2x2.py
@@ -1,5 +1,6 @@
 
 a = [[(8,8),(1,10)],[(10,1),(2,2)]]
+lista = []
 
 print (a [0]) #primera parte de la lista
 
@@ -16,101 +17,79 @@ print(a[0][1][0])# primera parte de la lista, segunda tupla, primer elemento
 
 
 # compara 8 vs 1
+# no se que argumento recibiria esto 
+def comparar_los_x ():
+    if a[0][0][0] > a[0][1][0]:
+        lista.append(a[0][0][0])
+    #print ("los Xi candidatos a equilibrio de nash son:"a[0][0][0])
 
-if a[0][0][0] > a[0][1][0]:
+    else:
+        lista.append(a[0][1][0])
+    #print("los Xi candidatos a equilibrio de nash son:"a[0][1][0])
 
-    print ("los Xi candidatos a equilibrio de nash son:"a[0][0][0])
+    # compara 8 vs 10
 
-else:
+    if a[0][0][0] > a[1][0][0]:
+        lista.append(a[0][0][0])
+        #print (a[0][0][0])
+    else:
+        lista.append(a[1][0][0])
+        #print(a[1][0][0])
 
-    print("los Xi candidatos a equilibrio de nash son:"a[0][1][0])
+    #compara 2 vs 1
+    if a[1][1][0] > a[0][1][0]:
+        lista.append(a[1][1][0])
+        #print (a[1][1][0])
+    else:
+        lista.append(a[0][1][0])
+        #print(a[0][1][0])
+        
+    #compara 2 vs 10
+    if a[1][1][0] > a[1][0][0]:
+        lista.append(a[1][1][0])
+        #print (a[1][1][0])
+    else:
+        lista.append(a[1][0][0])
 
-    
-
-# compara 8 vs 10
-
-    
-
-if a[0][0][0] > a[1][0][0]:
-
-    print (a[0][0][0])
-
-else:
-
-    print(a[1][0][0])
-
-    
-
-#compara 2 vs 1
-
-if a[1][1][0] > a[0][1][0]:
-
-    print (a[1][1][0])
-
-else:
-
-    print(a[0][1][0])
-
-
-
-#compara 2 vs 10
-
-if a[1][1][0] > a[1][0][0]:
-
-    print (a[1][1][0])
-
-else:
-
-    print(a[1][0][0])
+    #print(a[1][0][0])
+    print lista
 
 
+######################################
 
 # comparar los Yi
 
-
-
-#compara 8 vs 1
-
-if a[0][0][1] > a[1][0][1]:
-
-    print ("los Yi candidatos a equilibrio de nash son:"a[0][0][1])
-
-else:
-
-    print("los Yi candidatos a equilibrio de nash son:"a[1][0][1])
-
-
-
-#compara 8 vs 10
-
-if a[0][0][1] > a[0][1][1]:
-
-    print (a[0][0][1])
-
-else:
-
-    print(a[0][1][1])
-
-
-
-#compara 2 vs 1
-
-if a[1][1][1] > a[1][0][1]:
-
-    print (a[1][1][1])
-
-else:
-
-    print(a[1][0][1])
-
-
-
-#compara 2 vs 10
-
-if a[1][1][1] > a[0][1][1]:
-
-    print (a[1][1][1])
-
-else:
-
-    print(a[0][1][1])
+def comparar_los_y ():
+    #compara 8 vs 1
+    if a[0][0][1] > a[1][0][1]:
+        lista.append(a[0][0][1])
+        #print ("los Yi candidatos a equilibrio de nash son:"a[0][0][1])
+    else:
+        lista.append(a[1][0][1])
+        #print("los Yi candidatos a equilibrio de nash son:"a[1][0][1])
+        
+    #compara 8 vs 10
+    if a[0][0][1] > a[0][1][1]:
+        lista.append(a[0][0][1])
+        #print (a[0][0][1])
+    else:
+        lista.append(a[0][1][1])
+        #print(a[0][1][1])
+        
+    #compara 2 vs 1
+    if a[1][1][1] > a[1][0][1]:
+        lista.append(a[1][1][1])
+        #print (a[1][1][1])
+    else:
+        lista.append(a[1][0][1])
+        #print(a[1][0][1])
+        
+    #compara 2 vs 10
+    if a[1][1][1] > a[0][1][1]:
+        lista.append(a[1][1][1])
+        #print (a[1][1][1])
+    else:
+        lista.append(a[0][1][1])
+        #print(a[0][1][1])
+    print lista
+    
