ecfc74e71d1c5db9c5ce08422b3a8d2ae8963adb
ingridmejia <50503752+ingridmejia@users.noreply.github.com>
2019-05-21 13:01:49 -0500

    Update codigo_total.py

--- codigo_total.py
+++ codigo_total.py
@@ -1,57 +1,63 @@
-import numpy as np
-a =[]
+a =[[[1,0],[0,1],[2,1]],[[0,0],[2,4],[4,3]],[[2,1],[1,0],[3,2]]]
+
 lista = []
 respuesta = ""
-while respuesta.lower()!="no":
-    fila=a.append(input("Ingrese la fila de la siguiente manera [[a,b],[c,d]...[y,z]]:"))
-    respuesta=input(" Si desea ingresar otra fila diga 'si' de lo contrario diga 'no':")
-print (a)
+#while respuesta.lower()!="no":
+   # fila=a.append(input("Ingrese la fila de la siguiente manera [[a,b],[c,d]...[y,z]]:"))
+   # respuesta=input(" Si desea ingresar otra fila diga 'si' de lo contrario diga 'no':")
+
 
 # aqui identifica estrategias de dominancia estricta para reducir a una matriz 2x2
-########
-#dudas too los cositos van en parentesis ?
+
+
+b=len(a)
+c=len(a[0])
+
+def borrarfilas(a,x):
+    del a[x]
+def borraColumnas(a,y):
+    for i in range(len(a)):
+        del a[i][y]
+    
 def comparar_filas(i,l):
-    cond == a[i][0][0] >= a[l][0][0]
+    cond = a[i][0][0] >= a[l][0][0]
+   
     for j in range (1,b-1):
-        cond=cond ^ a[i][j][0] >= a[l][j][0]
+        cond=cond and a[i][j][0] >= a[l][j][0]
         
         if cond == True :
-            np.delete(a,l,axis=1)
+            borrarfilas(a,l)
+        
             #ponga_fila-enceros(l)
         else:
-            cond == a[i][0][0] < a[l][0][0]
-            cond=cond ^ a[i][j][0] < a[l][j][0]
-            np.delete(a,i,axis=1)
+            cond = a[i][0][0] < a[l][0][0]
+            cond=cond and a[i][j][0] < a[l][j][0]
+            borrarfilas(a,i)
             #ponga_fila_en_x(i)
             
 def comparar_columnas(i,l):
-    cond == a[i][0][1] >= a[l][0][1]
-    for j in range (1,b-1):
-        cond=cond ^ a[i][j][1] >= a[l][j][1]
+    cond = a[i][0][1] >= a[l][0][1]
+   
+    for j in range (1,c-1):
+        cond=cond and a[i][j][1] >= a[l][j][1]
         
         if cond == True :
-            np.delete(a,l,axis=1)
-            #ponga_columna_en_x(l) ## en vez de esto lo elimino usando la funcion de numpy 
+        
+            borraColumnas(a,l-1)
+            
+            
         else:
-            cond == a[i][0][1] < a[l][0][1]
-            cond=cond ^ a[i][j][1] < a[l][j][1]
-            np.delete(a,i,axis=1)
-            #ponga_columna_en_ceros(i)
-    #es mejor ponerlas en x para luego poderlas eliminar con  remove
-    # es mjor eliminarlas de una para ir reduciendo la matriz 
-
+            cond = a[i][0][1] < a[l][0][1]
+            cond=cond and a[i][j][1] < a[l][j][1]
+            borraColumnas(a,i)
+            
+           
  
 
 
 # PARA COMPARAR TODAS LAS FILAS CON CADA FILA MENOS CON SI MISMA PARA ELIMINAR POR DOMINANCIA ESTRICTA
     
-for i in range (0,c-2):
-    for l in range (i+1,c-1):
-        comparar_filas(i,l)
-        
-for i in range (0,c-2):
-    for l in range (i+1,c-1):
-        comparar_columnas(i,l)
+
         
 #hasta aqui reduce a una matrioz 2*2        
 ###############################
@@ -99,7 +105,7 @@ def comparar_los_x ():
         lista.append(a[1][0][0])
 
     #print(a[1][0][0])
-    print (lista)
+    #print (lista)
 
 
 ######################################
@@ -138,34 +144,58 @@ def comparar_los_y ():
     else:
         lista.append(a[0][1][1])
         #print(a[0][1][1])
-    print (lista)
+    #print (lista)
   
 def equilibrios ():
     final=[]
+    
+    
     for i in range (0,int(len(lista)/2)):
         for j in range (int(len(lista)/2),int(len(lista))):
             lista1 =[] 
             lista1.append(lista[i])
             lista1.append(lista[j])
-            print(lista1)
+            #print(lista1)
             for z in range (2):
                 for p in range(2):
-                    print(a[z][p])
                     if lista1 == a[z][p]:
                         final.append(lista1)
-    print(final)
-    if len(final)==0:
-        print ("no hay equilibrios de nash")
-    else:
-        print ("el equilibrio de nash es:" )
-        print(final)
-    return final
+    
+    #aqui elimino las sublistas repetidas
+    listanueva=[]
+    for h in final:
+        if h not in listanueva:
+            listanueva.append(h)
     
             
     
+    
+    if len(listanueva)==0:
+        print ("no hay equilibrios de nash")
+    else:
+        print ("el equilibrio de nash es:" )
+        print(listanueva)
+    return listanueva
 
+    
+#EJECUCION  
+print("matriz inicial")
+print (a)
+print()  
+    
+for i in range (0,b-2):
+    for l in range (i+1,b-1):
+        comparar_filas(i,l)
+        print("elimino fila")
+        print(a)
+        print()
+        
+for i in range (0,c-2):
+    for l in range (i+1,c-1):
+        comparar_columnas(i,l)
+        print("elimino columna")
+        print(a)
+        print()
+        
 comparar_los_x() # esto me da los xi 
 comparar_los_y() # esto me da los xi y yi 
-equilibrios ()
-#para poder comparar y hacer los equilibrios tocacambiar la idea de poner los puntos como tuplas, por ponerlos como listas 
-#el codigo final contempla desde el inicio listas de listas de listas.
